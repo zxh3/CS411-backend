40c36633cf75b711058850822db496ab2ac8e515
Xiaohua <cheonghiuwaa@gmail.com>
2018-10-30 03:43:23 -0500

    stage4

--- index.js
+++ index.js
@@ -42,5 +42,90 @@ app.delete('/dishes/:dishName', (req, res) => {
   });
 })
 
+app.get('/ingredients/:dishName', (req, res) => {
+  const q = `SELECT ingredientName FROM dishIngredient WHERE dishName='${req.params.dishName}'`;
+  con.query(q, (err, results) => {
+    if (!err) {
+      console.log(results);
+      res.json(results);
+    } else {
+      res.json({error: err});
+    }
+  });
+});
+
+app.get('/restaurants/:dishName', (req, res) => {
+  const q = `SELECT restaurantName FROM dishRestaurant WHERE dishName='${req.params.dishName}'`;
+  con.query(q, (err, results) => {
+    if (!err) {
+      console.log(results);
+      res.json(results);
+    } else {
+      res.json({error: err});
+    }
+  })
+});
+
+app.post('/addDish', (req, res) => {
+  let { dishName, ingredients } = req.body;
+  let dishIngredient = ingredients.map(ingredient => [dishName, ingredient]);
+  ingredients = ingredients.map(x => [x]);
+  console.log('dishName: ', dishName);
+  console.log('ingredients: ', ingredients);
+  console.log('dishIngredient: ', dishIngredient);
+  console.log(dishName, ingredients);
+
+  let q = `SELECT * FROM dish WHERE name='${dishName}'`;
+  con.query(q, (err, results) => {
+    console.log('results: ', results);
+    if (!err) {
+      if (results && results.length > 0 ) {
+        res.json({error: `Dish already exists.`});
+      } else {
+        
+        let q1 = `INSERT INTO dish VALUES ('${dishName}');`;
+        let q2 = `INSERT IGNORE INTO ingredient (name) VALUES ?`;
+        let q3 = `INSERT IGNORE INTO dishIngredient (dishName, ingredientName) VALUES ?`;
+
+        con.query(q1, (err) => {
+          if (!err) {
+            con.query(q2, [ingredients], (err) => {
+              if (!err) {
+                con.query(q3, [dishIngredient], (err) => {
+                  if (!err) {
+                    console.log('success addDish');
+                    res.json({success: 0});
+                    return;
+                  } else {
+                    res.json({error: err});
+                  }
+                })
+              } else {
+                res.json({error: err});
+              }
+            })
+          } else {
+            res.json({error: err});
+          }
+        });
+      }
+    } else {
+      res.json({error: err});
+    }
+  });
+});
+
+app.put('/dishes', (req, res) => {
+  const oldName = req.body.oldName;
+  const newName = req.body.newName;
+  const q = `UPDATE dish SET name='${newName}' WHERE name='${oldName}'`;
+  con.query(q, (err) => {
+    if (!err) {
+      res.json({success: 0});
+    } else {
+      res.json({error: err});
+    }
+  });
+});
 
 app.listen(2018, () => console.log(`listening on port 2018`));
