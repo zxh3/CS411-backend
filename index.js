10432e472ce85c2f69b23f7ec47e50be2f51735a
Xiaohua <cheonghiuwaa@gmail.com>
2018-10-30 14:01:47 -0500

    handle disconnect

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ const db_config = {
 let con = mysql.createConnection(db_config);
 
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  con = mysql.createConnection(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
+  con.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
+  con.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
@@ -41,13 +41,6 @@ function handleDisconnect() {
 }
 
 handleDisconnect();
-
-
-con.connect((err) => {
-  if (err) {
-    console.error('error !!!', err);
-  }
-});
 
 app.get('/', (req, res) => {
   res.json({eatmoodBackend: 'yoxi'});
